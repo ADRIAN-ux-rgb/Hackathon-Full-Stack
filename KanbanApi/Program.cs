4a92860cc544e19cb51b1303ccf7ae7427dd1a01
ADRIAN-ux-rgb <adriankaua294@gmail.com>
2026-05-16 17:39:53 -0300

    feat: integração do frontend com API

--- KanbanApi/Program.cs
+++ KanbanApi/Program.cs
@@ -1,3 +1,5 @@
+using System.Security.Authentication.ExtendedProtection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -5,11 +7,23 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(FileOptions =>
+{
+    FileOptions.AddPolicy("PermitirFrontend", policy =>
+    {
+         policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+        
+    });
+});
+
 var app = builder.Build();
 
 app.UseSwagger();
-
 app.UseSwaggerUI();
+
+app.UseCors("PermitirFrontend");
 
 app.UseAuthorization();
 
